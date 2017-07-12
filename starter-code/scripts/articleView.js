25af8046f75733da1a7e2816d92f462ef02dd87a
Meepedy <david@psyon.org>
2017-07-12 14:22:22 -0500

    added acccent colors to links

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -83,7 +83,6 @@ articleView.setTeasers = function() {
 
 };
 
-// TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
